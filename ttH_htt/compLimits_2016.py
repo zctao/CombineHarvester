cf59ad0b2dba573ff924020b986f041986ac4783
Ram <ram.krishna.dewanjee@cern.ch>
2018-02-19 14:13:25 +0200

    Added the 2los_1tau channel and the modified compLimits_2016.py

--- ttH_htt/compLimits_2016.py
+++ ttH_htt/compLimits_2016.py
@@ -1,58 +1,26 @@
 import os, subprocess, sys
 
-channels = [
-  ##"1l_2tau_LLR",
-  ##"1l_2tau_ND",
-  "1l_2tau_Tallinn",
-  ##"2lss_1tau_Cornell",
-  ##"2lss_1tau_LLR",
-  "2lss_1tau_Tallinn",
-  ##"3l_1tau_LLR",
-  "3l_1tau_Tallinn",
-]
+## ---- Add the channel name here as a new string member of the array-- ##
+channels = [ "2los_1tau"]
 
+## ---- Add the final observable  name (used in your channel) here ---- ##
 shapeVariables = {
-  "1l_2tau_LLR"       : [ "mTauTauVis" ],
-  "1l_2tau_ND"        : [ "mTauTauVis" ],
-  "1l_2tau_Tallinn"   : [ "mTauTauVis" ],
-  "2lss_1tau_Cornell" : [ "mvaDiscr_2lss" ],
-  "2lss_1tau_LLR"     : [ "mvaDiscr_2lss" ],
-  "2lss_1tau_Tallinn" : [ "mvaDiscr_2lss" ],
-  "3l_1tau_LLR"       : [ "mvaDiscr_3l" ],
-  "3l_1tau_Tallinn"   : [ "mvaDiscr_3l" ],
+  "2los_1tau" : [ "mvaDiscr_2los" ], 
 }
 
+## ---- Add the path to your datacard root file name that you have copied to "shapes/" dir. ---- ##
 datacardFiles = {
-  "1l_2tau_LLR"       : "LLR/datacard_mvis_1l2tau_syst_12.9fb_2017Jan13.root",
-  "1l_2tau_ND"        : "ND/sync_workspace.root",
-  "1l_2tau_Tallinn"   : "Tallinn/datacard_mvis_1l2tau_12.9fb_2017Jan13.root",
-  "2lss_1tau_Cornell" : "Cornell/ttH_2lss_1tau.input.root",
-  "2lss_1tau_LLR"     : "LLR/datacard_MVA_2lSS_1tau_nofaketau_syst_12.9fb_2017Jan13.root",
-  "2lss_1tau_Tallinn" : "Tallinn/datacard_MVA_2lSS_1tau_nofaketau_12.9fb_2017Jan13.root",
-  "3l_1tau_LLR"       : "LLR/datacard_MVA_3l_1tau_12.9fb_2017Jan13.root",
-  "3l_1tau_Tallinn"   : "Tallinn/datacard_MVA_3l_1tau_nofaketau_12.9fb_2017Jan13.root",
+  "2los_1tau" : "KBFI/datacards_2los_1tau.root",
 }
 
+## ----- Add the executable name here ---- ####
 WriteDatacard_executables = {
-  "1l_2tau_LLR"       : 'WriteDatacards_1l_2tau',
-  "1l_2tau_ND"        : 'WriteDatacards_1l_2tau',
-  "1l_2tau_Tallinn"   : 'WriteDatacards_1l_2tau',
-  "2lss_1tau_Cornell" : 'WriteDatacards_2lss_1tau',
-  "2lss_1tau_LLR"     : 'WriteDatacards_2lss_1tau',
-  "2lss_1tau_Tallinn" : 'WriteDatacards_2lss_1tau',
-  "3l_1tau_LLR"       : 'WriteDatacards_3l_1tau',
-  "3l_1tau_Tallinn"   : 'WriteDatacards_3l_1tau',
+  "2los_1tau" : 'WriteDatacards_2los_1tau', 
 }
 
+## ------ Add your postfit plot macro here ---- ###
 makePostFitPlots_macros = {
-  "1l_2tau_LLR"       : 'makePostFitPlots_1l_2tau.C',
-  "1l_2tau_ND"        : 'makePostFitPlots_1l_2tau.C',
-  "1l_2tau_Tallinn"   : 'makePostFitPlots_1l_2tau.C',
-  "2lss_1tau_Cornell" : 'makePostFitPlots_2lss_1tau.C',
-  "2lss_1tau_LLR"     : 'makePostFitPlots_2lss_1tau.C',
-  "2lss_1tau_Tallinn" : 'makePostFitPlots_2lss_1tau.C',
-  "3l_1tau_LLR"       : 'makePostFitPlots_3l_1tau.C',
-  "3l_1tau_Tallinn"   : 'makePostFitPlots_3l_1tau.C',
+  "2los_1tau" : 'makePostFitPlots_2los_1tau.C', 
 }    
 
 def run_cmd(command):
@@ -62,30 +30,44 @@ def run_cmd(command):
   return stdout
 
 workingDir = os.getcwd()
-datacardDir = "/home/veelken/public/HIG16022_datacards/"
+datacardDir = "shapes/" ## NEW !
 
 for channel in channels:
   for shapeVariable in shapeVariables[channel]:
     channel_base = None
-    for search_string in [ "1l_2tau", "2lss_1tau", "3l_1tau" ]:
+    for search_string in [ "1l_2tau", "2lss_1tau", "3l_1tau", "2los_1tau", "2l_2tau", "2los_2tau", "2lss_2tau" ]: 
       if channel.find(search_string) != -1:
         channel_base = search_string
+        datacard_dir = os.path.join(workingDir, "datacards")
+        if not os.path.exists(datacard_dir):
+          os.makedirs(datacard_dir)
+        if not os.path.exists(os.path.join(datacard_dir, channel)):
+          os.makedirs( os.path.join(datacard_dir, channel) )
+          os.makedirs( os.path.join(datacard_dir, channel, "logs") )
+        postfit_plot_dir = os.path.join(workingDir, "macros", "plots")
+        if not os.path.exists(postfit_plot_dir):
+          os.makedirs(postfit_plot_dir)
     ##datacardFile_input = os.path.join(datacardDir, channel_base, datacardFiles[channel] % shapeVariable)
     datacardFile_input = os.path.join(datacardDir, datacardFiles[channel])
-    datacardFile_output = os.path.join(workingDir, "limits", "ttH_%s.root" % channel)
-    run_cmd('rm ttH_%s.txt' % channel)
-    run_cmd('rm %s' % datacardFile_output)
-    run_cmd('%s --input_file=%s --output_file=%s --add_shape_sys=false' % (WriteDatacard_executables[channel], datacardFile_input, datacardFile_output))
-    txtFile = datacardFile_output.replace(".root", ".txt")
-    run_cmd('cp %s %s' % (datacardFile_output, datacardFile_output.replace("/limits/", "/")))
+    datacardFile_path = os.path.join(workingDir, "datacards", channel)
+    datacardRootFile_output = os.path.join(datacardFile_path, "ttH_%s.root" % channel)
+    datacardTxtFile_output = datacardRootFile_output.replace(".root", ".txt")
+    logFile_limits = os.path.join(datacardFile_path, "logs", "ttH_%s_%s_2016_limits.log" % (channel, shapeVariable))
+    logFile_mlfit = os.path.join(datacardFile_path, "logs", "ttH_%s_%s_2016_mlfit.log" % (channel, shapeVariable))
+    logFile_postfit_shapes = os.path.join(datacardFile_path, "logs", "ttH_%s_%s_2016_postfit_shapes.log" % (channel, shapeVariable))
+    run_cmd('rm %s' % datacardRootFile_output)
+    run_cmd('rm %s' % datacardTxtFile_output)
+    run_cmd('%s --input_file=%s --output_file=%s --add_shape_sys=true' % (WriteDatacard_executables[channel], datacardFile_input, datacardRootFile_output))
+    ### run_cmd('cp %s %s' % (datacardRootFile_output, datacardRootFile_output.replace("/datacards/", "/")))
+    run_cmd('rm %s' % logFile_mlfit)    
     ##run_cmd('combine -M MaxLikelihoodFit -t -1 -m 125 %s' % txtFile)
-    run_cmd('combine -M MaxLikelihoodFit -m 125 %s' % txtFile)
-    logFile = os.path.join(workingDir, "limits", "ttH_%s_%s_2016.log" % (channel, shapeVariable))
-    run_cmd('rm %s' % logFile)
+    run_cmd('combineTool.py -M MaxLikelihoodFit -m 125 -d %s --there &> %s' % (datacardTxtFile_output, logFile_mlfit))
+    run_cmd('rm %s' % logFile_limits)    
     ##run_cmd('combine -M Asymptotic -t -1 -m 125 %s &> %s' % (txtFile, logFile))
-    run_cmd('combine -M Asymptotic -m 125 %s &> %s' % (txtFile, logFile))
-    rootFile = os.path.join(workingDir, "limits", "ttH_%s_%s_2016_shapes.root" % (channel_base, shapeVariable))
-    run_cmd('rm %s' % rootFile)
-    run_cmd('PostFitShapes -d %s -o %s -m 125 -f mlfit.root:fit_s --postfit --sampling --print' % (txtFile, rootFile))
-    ##run_cmd('root -b -n -q -l %s++' % os.path.join(workingDir, "macros", makePostFitPlots_macros[channel]))
+    run_cmd('combineTool.py -M Asymptotic -m 125 --there -d %s &> %s' % (datacardTxtFile_output, logFile_limits))
+    PostFitShapesRootFile = os.path.join(workingDir, "datacards", channel, "ttH_%s_%s_2016_shapes.root" % (channel_base, shapeVariable))
+    run_cmd('rm %s' % logFile_postfit_shapes)    
+    run_cmd('rm %s' % PostFitShapesRootFile)
+    run_cmd('PostFitShapes -d %s -o %s -m 125 -f %s/mlfit.Test.root:fit_s --postfit --sampling --print &> %s' % (datacardTxtFile_output, PostFitShapesRootFile, datacardFile_path, logFile_postfit_shapes))
+    run_cmd('root -b -n -q -l %s++' % os.path.join(workingDir, "macros", makePostFitPlots_macros[channel]))
 
